51b706508b1d7a3b5e6c4cc2e688402448c7121f
Anshu Kumar <anshuak8515@gmail.com>
2023-09-25 11:49:55 +0530

    Table

--- multiplication.java
+++ multiplication.java
@@ -4,11 +4,9 @@ import java.util.Scanner;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number:  ");
         int n = sc.nextInt();
-        {
             for(int i=1; i<=10; i++){
             int t = n*i;
             System.out.println(t);
             }
-        }
   }
  }
--- sum.java
+++ sum.java
@@ -1,8 +1,7 @@
-import java.util.*;
 class Main{
     public static void main(String args[]){
-        int a=10;
-        int b=20;
+        int a = 10;
+        int b = 20;
         System.out.println(a+b);
 
     }
