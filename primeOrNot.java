7b71c0a188ef1c67c1fb6a1e81b4773ae568b945
Anshu Kumar <anshuak8515@gmail.com>
2023-09-28 23:49:15 +0530

    Prime Or Not

--- primeOrNot.java
+++ primeOrNot.java
@@ -1,14 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 public class primeOrNot {
     public static void main(String args[] ){
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter the number:  ");
     int num = sc.nextInt();
-    if(num % 1 == 0 && num % num == 0)
-        System.out.println("Prime");
+    int temp = 0;
+    for(int i=2; i<=num-1; i++)
+    {
+        if (num%i==0){
+            temp=temp+1; 
+         }   
+    } 
+    if(temp==0)         
+            System.out.println(num + " is Prime.");
     else
     {
-        System.out.println("Not Prime");
+        System.out.println(num + " is Not Prime.");
     }
 }
 }
